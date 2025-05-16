f91e507952df96188ce76e25c5c407a816760152
Tilak <tilak972003@gmail.com>
2025-05-16 15:03:57 +0530

    Add project files.

---
+++ Controllers/PortfolioController.cs
@@ -0,0 +1,50 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.IO;
+
+namespace PortfolioWebsite.Controllers
+{
+    public class PortfolioController : Controller
+    {
+        public IActionResult Home()
+        {
+            return View();
+        }
+
+        public IActionResult About()
+        {
+            return View();
+        }
+
+        public IActionResult Projects()
+        {
+            return View();
+        }
+
+        public IActionResult Skills()
+        {
+            return View();
+        }
+
+        public IActionResult Certificates()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult DownloadResume()
+        {
+            var filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "files", "My Resume placement.pdf");
+            if (!System.IO.File.Exists(filePath))
+            {
+                return NotFound("Resume file not found.");
+            }
+
+            var fileBytes = System.IO.File.ReadAllBytes(filePath);
+            return File(fileBytes, "application/pdf", "My Resume placement.pdf");
+        }
+    }
+}
